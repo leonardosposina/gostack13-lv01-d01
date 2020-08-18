48516ef6c91add77050b3edae3ad7dde5ae69fd1
Leonardo Sposina <leonardo.sposina@icloud.com>
2020-08-18 11:05:04 -0300

    challenge submit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rocketseat
+
+## Bootcamp GoStack #13
+
+### Level 01 - Desafio Conceitos Node.js
+
+- [Instruções](https://github.com/rocketseat-education/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs)
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,73 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  const { title, url, techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repo);
+
+  return response.status(201).json(repo);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoId = repositories.findIndex( (repo) => repo.id === id );
+
+  if (repoId < 0)
+    return response.status(400).send();
+
+  const repo = Object.assign(repositories[repoId], { title, url, techs });
+  
+  repositories[repoId] = repo;
+  
+  return response.status(200).json(repo);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+  const repoId = repositories.findIndex( (repo) => repo.id === id );
+
+  if (repoId < 0)
+    return response.status(400).send();
+
+  repositories.splice(repoId, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+  const repoId = repositories.findIndex( (repo) => repo.id === id );
+
+  if (repoId < 0)
+    return response.status(400).send();
+
+  repositories[repoId].likes++;
+
+  return response.status(200).json(repositories[repoId]);
+
 });
 
 module.exports = app;
